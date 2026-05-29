3b6e8ea940da4f3963fab61f423ef6f5531fb590
Danial Bakhtiari <danial.bakhtiari96@gmail.com>
2026-05-29 12:59:31 +0330

    Newest & TwoBanner & Equipment & Footer Finished

--- src/js/app.js
+++ src/js/app.js
@@ -29,3 +29,31 @@ const swiperTwo = new Swiper(".mySwiperTwo", {
     prevEl: ".swiper-button-prev",
   },
 });
+const swiperThree = new Swiper(".mySwiperThree", {
+  slidesPerView: 5,
+  spaceBetween: 0,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+const swiperFour = new Swiper(".mySwiperFour", {
+  slidesPerView: 5,
+  spaceBetween: 0,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
